7b55af0782f3c3212eae547b3b663099e050ff4f
Fisch <112293434+F2sh-F2sh@users.noreply.github.com>
2026-06-17 11:20:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Website loaded");
+
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const SUPABASE_URL = "https://bzcarceljgdwdqlcbotl.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_VNz-PocQFPrNjsap1Asw2g_s7-AkKb4";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
